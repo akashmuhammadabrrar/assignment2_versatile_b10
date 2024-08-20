1f09a4e02541e1dafd52828927f26538c4a6b6b6
AkashMAbrrar <madborakash48@gmail.com>
2024-08-20 13:46:32 +0600

    thsi commti for the last section footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export const content = ["./src/**/*.{html,js}"];
 export const theme = {
